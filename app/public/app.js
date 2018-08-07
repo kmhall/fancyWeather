7e651712b6dfab72f08bdf206a26831a2df345c9
Kyle Hall <kyle.hall@showpad.com>
2018-08-07 09:54:16 -0500

    timer

--- app/public/app.js
+++ app/public/app.js
@@ -1,5 +1,6 @@
 //Initial Load
 window.onload = function initialLoad() {
+    startTime();
     currentLocation = {
         city: "",
         country: ""
@@ -50,6 +51,22 @@ window.onload = function initialLoad() {
             getWeatherPhoto(weatherType);
         });
     }, "jsonp");
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
 }
 
 function getWeatherPhoto(weatherType) {
