5d30fa5747922cd2418e21f4fcd1e5ab71d9e024
Kyle Hall <kylehall484@gmail.com>
2018-08-19 15:07:07 -0500

    Revert "Added support for switching to celsius "

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,26 @@
-var weather = require("weather-js");
+var weather = require('weather-js');
 
-// set initial degree type to Farenheit
-let degreePreference = "F";
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/weather/", function(req, res) {
+  app.get("/api/weather/", function (req, res) {
     loc = req.param("loc");
-    degreePreference = req.param("tempType");
-
     console.log(loc);
 
-    weather.find({ search: loc, degreeType: degreePreference }, function(
-      err,
-      result
-    ) {
+    weather.find({ search: loc, degreeType: 'F' }, function (err, result) {
       if (err) console.log(err);
 
-      // console.log(JSON.stringify(result, null, 2));
+      // console.log(JSON.stringify(result, null, 2));      
       res.json(result);
+
     });
   });
-};
+}
+
+
+
